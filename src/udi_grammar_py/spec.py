d2331201d91ec34414529a5e1dc5056c59aa65a2
dev-lan <devinscottlange@gmail.com>
2025-03-10 14:50:25 -0600

    add basic representation class

--- src/udi_grammar_py/spec.py
+++ src/udi_grammar_py/spec.py
@@ -1,4 +1,7 @@
+
 import json
+
+PRETTY_INDENT = 4
 
 class Chart:
 
@@ -12,5 +15,42 @@ class Chart:
     # def add_representation(self, name, representation):
     #     self.spec['representation'].append({'name': name
 
-    def to_json(self):
-        return json.dumps(self._spec)
+    def add_layer(self):
+        layer = Layer()
+        self._spec['representation'].append(layer)
+        return layer
+
+    def to_json(self, pretty=False):
+        # handle inner object serialization
+        def custom_serialization(obj):
+            if isinstance(obj, Layer):
+                return obj.__json__()
+            raise TypeError(f"Type {type(obj)} not serializable")
+
+        return json.dumps(self._spec, default=custom_serialization, indent=PRETTY_INDENT if pretty else None)
+    
+    def to_dict(self):
+        return json.loads(self.to_json())
+
+class Layer:
+    def __init__(self):
+        self._state = { 'mark': None, 'mapping': [] }
+
+    def mark(self, mark: str):
+        self._state['mark'] = mark
+        return self
+
+    def map(self, encoding: str, **kwargs):
+        new_mapping = { 'encoding': encoding }
+        if 'field' in kwargs:
+            new_mapping['field'] = kwargs['field']
+        if 'type' in kwargs:
+            new_mapping['type'] = kwargs['type']
+        if 'value' in kwargs:
+            new_mapping['value'] = kwargs['value']
+        self._state['mapping'].append(new_mapping)
+        return self
+    
+    def __json__(self):
+        # Return a dictionary that can be serialized by json
+        return self._state
--- tests/test_spec.py
+++ tests/test_spec.py
@@ -21,3 +21,42 @@ def test_chart_to_json():
     chart.source('test_name', 'test_source')
     expected_json = '{"source": [{"name": "test_name", "source": "test_source"}], "representation": []}'
     assert chart.to_json() == expected_json
+
+def test_chart_to_dict():
+    chart = Chart()
+    chart.source('test_name', 'test_source')
+    expected_dict = {'source': [{'name': 'test_name', 'source': 'test_source'}], 'representation': []}
+    assert chart.to_dict() == expected_dict
+
+def test_scatterplot():
+    chart = Chart()
+    chart.source('donors', './data/donors.csv') \
+         .add_layer() \
+         .mark('point') \
+         .map(encoding='x', field='weight', type='quantitative') \
+         .map(encoding='y', field='height', type='quantitative')
+    assert chart.to_dict() == {
+        "source": [
+            {
+                "name": "donors",
+                "source": "./data/donors.csv"
+            }
+        ],
+        "representation": [
+            {
+                "mark": "point",
+                "mapping": [
+                    {
+                        "encoding": "x",
+                        "field": "weight",
+                        "type": "quantitative"
+                    },
+                    {
+                        "encoding": "y",
+                        "field": "height",
+                        "type": "quantitative"
+                    }
+                ]
+            }
+        ]
+    }
